f18ff4b47e80e8811eea51fd96a5222ba7d379a0
SugarFreeDoNoSo <matias.donoso.q@gmail.com>
2025-02-24 20:00:44 -0300

    ui brutal

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
-  output: 'static',
+  output: 'server',
+  adapter: vercel({})
 })
